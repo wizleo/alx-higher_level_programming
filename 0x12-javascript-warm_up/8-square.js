36a9e640a3417d40708ccf2b7b0f627a923319d1
wizleo <nkajjasolomon420@gmail.com>
2022-10-04 06:06:39 +0000

    corrections

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,9 +3,9 @@ const size = Math.floor(Number(process.argv[2]));
 if (isNaN(size)) {
 	console.log('Missing size');
 } else {
-	for (let i = 0; i < size; i++) {
-		let square = '';
-		for (let j = 0; j < size; j++) square += 'X';
-		console.log(square);
+	for (let r = 0; r < size; r++) {
+		let row = '';
+		for (let c = 0; c < size; c++) row += 'X';
+		console.log(row);
 	}
 }
