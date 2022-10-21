908d4726027a37683a2844f055aa5a81d652f8b2
wizleo <nkajjasolomon420@gmail.com>
2022-10-21 14:42:50 +0000

    correction

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 """
-    A script that fetches https://intranet.htbn.io/status
+Fetches the URL: https://intranet.hbtn.io/status
 """
 
-
-import urllib.request
+from urllib.request import Request, 
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
-        html = resp.read()
+    req = Request('https://alx-intranet.hbtn.io/status')
 
-        print("Body response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode('utf8')))
+    with urlopen(req) as res:
+        content = res.read()
+        utf8_content = content.decode('utf-8')
+
+        print('Body response:')
+        print('\t- type: {_type}'.format(_type=type(content)))
+        print('\t- content: {_content}'.format(_content=content))
+        print('\t- utf8 content: {_utf8_c}'.format(_utf8_c=utf8_content))
