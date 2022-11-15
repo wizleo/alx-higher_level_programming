0fbb9d0bc758ff1ff01b8ba4ffaa3bbb32340fd7
wizleo <nkajjasolomon420@gmail.com>
2022-11-15 18:25:36 +0000

    0x14-javascript-web_scraping correction

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,18 +1,4 @@
 #!/usr/bin/node
 const fs = require('fs');
 const request = require('request');
-
-const url = process.argv[2] || '';
-const filePath = process.argv[3];
-
-request(url, (err, res, body) => {
-	if (err) {
-		return console.log(err);
-	}
-
-	fs.writeFile(filePath, body, 'utf8', (err) => {
-		if (err) {
-			return console.log(err);
-		}
-	});
-});
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
