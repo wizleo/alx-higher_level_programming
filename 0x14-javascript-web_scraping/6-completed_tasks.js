92e6c37ce79cda528ba77471ab34cdb7c7e358c6
wizleo <nkajjasolomon420@gmail.com>
2022-11-15 18:29:16 +0000

    0x14-javascript-web_scraping correction

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,23 +1,25 @@
 #!/usr/bin/node
+// script that computes the number of tasks completed by user id
 const request = require('request');
-
-const url = process.argv[2] || '';
-const storage = {};
-let tmp = '';
-
-request(url, (err, res, body) => {
+const myArgs = process.argv.slice(2);
+const result = {};
+request(myArgs[0], function (err, response, body) {
 	if (err) {
-		return console.log(err);
-	}
-
-	const parsedBody = JSON.parse(body);
-
-	for (let i = 0; i < parsedBody.length; i++) {
-		tmp = parsedBody[i].userId;
-		if (parsedBody[i].completed) {
-			storage[tmp] = (storage[tmp] || 0) + 1;
+		console.log(err);
+	} else {
+		const JSONbody = JSON.parse(body);
+		let i = 0;
+		for (i = 0; i < JSONbody.length; i++) {
+			if (!(JSONbody[i].userId in result)) {
+				result[JSONbody[i].userId] = 0;
+			}
+			if (JSONbody[i].completed) {
+				result[JSONbody[i].userId] += 1;
+			}
+			if (result[JSONbody[i].userId] === 0) {
+				delete result[JSONbody[i].userId];
+			}
 		}
+		console.log(result);
 	}
-
-	console.log(storage);
 });
