42bb3d2e22a40f2ead6170816b756befaffe77ac
wizleo <nkajjasolomon420@gmail.com>
2022-11-15 18:24:25 +0000

    0x14-javascript-web_scraping correction

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,22 +1,12 @@
 #!/usr/bin/node
 const request = require('request');
-
-const url = process.argv[2];
-
-request(url, (err, res, body) => {
-	if (err) {
-		return console.log(err);
+request(process.argv[2], function (error, response, body) {
+	if (!error) {
+		const results = JSON.parse(body).results;
+		console.log(results.reduce((count, movie) => {
+			return movie.characters.find((character) => character.endsWith('/18/'))
+			  ? count + 1
+			  : count;
+		}, 0));
 	}
-
-	let count = 0;
-	const parsedBody = JSON.parse(body).results;
-
-	for (let i = 0; i < parsedBody.length; i++) {
-		const a = parsedBody[i].characters.find((c) => {
-			return c.match(/18/);
-		});
-		if (a !== undefined) {
-			count++;
-		}
-	}
-}
+});
